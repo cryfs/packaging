b5b4e27f513e6c40fd1288a36ff648f92bc7aa5c
Sebastian Messmer <mail@smessmer.de>
2015-11-24 23:18:50 +0100

    - Stop machines after building - Fix apt-get install lines

--- build.sh
+++ build.sh
@@ -11,5 +11,7 @@ cd ${0%/*}
 cd vagrant
 vagrant up $VIRTUAL_MACHINE
 vagrant ssh $VIRTUAL_MACHINE -c "/vagrant/build_package.sh https://github.com/cryfs/cryfs $TAG /vagrant/build/cryfs_${TAG}_${VIRTUAL_MACHINE}.deb"
+sleep 2
+vagrant halt $VIRTUAL_MACHINE
 
 set +e
