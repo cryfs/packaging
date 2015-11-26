ad9f6fa7c0d4d477b0138742f4c1dfac2a3bf881
Sebastian Messmer <mail@smessmer.de>
2015-11-27 00:12:07 +0100

    Shutdown VM also if there was an error

--- run_in_vm.sh
+++ run_in_vm.sh
@@ -8,13 +8,17 @@ set -e
 # Switch to directory of script
 cd ${0%/*}
 
+function cleanup {
+  # Try halting the machine until it is successful (vagrant sometimes fails halting machines :( )
+  until vagrant halt $VIRTUAL_MACHINE
+  do
+    sleep 5
+  done  
+}
+
 cd vagrant
+trap cleanup EXIT # run cleanup on exit of the bash script
 vagrant up $VIRTUAL_MACHINE
 vagrant ssh $VIRTUAL_MACHINE -c "$COMMAND"
-# Try halting the machine until it is successful (vagrant sometimes fails halting machines :( )
-until vagrant halt $VIRTUAL_MACHINE
-do
-  sleep 5
-done
 
 set +e
