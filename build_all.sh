e6d1607b82cdfb3ac658d5b45b35dd003af1f3f2
Sebastian Messmer <mail@smessmer.de>
2015-11-28 01:54:58 +0100

    Add Build/Test process for debian 8

--- build_all.sh
+++ build_all.sh
@@ -8,9 +8,11 @@ set -e
 ./build.sh ubuntu-15.04-x86_64 $TAG
 ./build.sh ubuntu-14.04-x86_64 $TAG
 ./build.sh ubuntu-12.04-x86_64 $TAG
+./build.sh debian-8-x86_64 $TAG
 ./build.sh ubuntu-15.10-x86 $TAG
 ./build.sh ubuntu-15.04-x86 $TAG
 ./build.sh ubuntu-14.04-x86 $TAG
 ./build.sh ubuntu-12.04-x86 $TAG
+./build.sh debian-8-x86 $TAG
 
 set +e
--- test_all.sh
+++ test_all.sh
@@ -8,9 +8,11 @@ set -e
 ./test.sh ubuntu-15.04-x86_64 $TAG
 ./test.sh ubuntu-14.04-x86_64 $TAG
 ./test.sh ubuntu-12.04-x86_64 $TAG
+./test.sh debian-8-x86_64 $TAG
 ./test.sh ubuntu-15.10-x86 $TAG
 ./test.sh ubuntu-15.04-x86 $TAG
 ./test.sh ubuntu-14.04-x86 $TAG
 ./test.sh ubuntu-12.04-x86 $TAG
+./test.sh debian-8-x86 $TAG
 
 set +e
