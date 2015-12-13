4c1a866f8393cc2a7deba04b7d32a74ef1126a1e
Sebastian Messmer <mail@smessmer.de>
2015-12-13 04:24:38 +0100

    Parallel deploy of digital_ocean/google doesn't seem to work. Make it sequential again

--- start_machines.sh
+++ start_machines.sh
@@ -6,8 +6,7 @@ set -e
 cd ${0%/*}
 
 cd vagrant
-vagrant up --provider=google ubuntu-15.10-x64 ubuntu-15.04-x64 ubuntu-14.04-x64 ubuntu-12.04-x64 debian-8-x64 &
-vagrant up --provider=digital_ocean ubuntu-15.10-x32 ubuntu-15.04-x32 ubuntu-14.04-x32 ubuntu-12.04-x32 &
-wait
+vagrant up --provider=google ubuntu-15.10-x64 ubuntu-15.04-x64 ubuntu-14.04-x64 ubuntu-12.04-x64 debian-8-x64
+vagrant up --provider=digital_ocean ubuntu-15.10-x32 ubuntu-15.04-x32 ubuntu-14.04-x32 ubuntu-12.04-x32
 
 set +e
