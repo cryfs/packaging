54d362fcb3680a1b5fa1020fd2d78bfc6ef88630
Sebastian Messmer <mail@smessmer.de>
2015-12-13 04:18:08 +0100

    - Improved README - Machines in ./start_machines.sh on digital ocean and google start in parallel

--- start_machines.sh
+++ start_machines.sh
@@ -6,7 +6,8 @@ set -e
 cd ${0%/*}
 
 cd vagrant
-vagrant up --provider=google ubuntu-15.10-x64 ubuntu-15.04-x64 ubuntu-14.04-x64 ubuntu-12.04-x64 debian-8-x64
-vagrant up --provider=digital_ocean ubuntu-15.10-x32 ubuntu-15.04-x32 ubuntu-14.04-x32 ubuntu-12.04-x32
+vagrant up --provider=google ubuntu-15.10-x64 ubuntu-15.04-x64 ubuntu-14.04-x64 ubuntu-12.04-x64 debian-8-x64 &
+vagrant up --provider=digital_ocean ubuntu-15.10-x32 ubuntu-15.04-x32 ubuntu-14.04-x32 ubuntu-12.04-x32 &
+wait
 
 set +e
