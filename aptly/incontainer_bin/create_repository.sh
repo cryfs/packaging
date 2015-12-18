a6a20cc5b9a70596100afb1c8b23b95e0fe52f81
Sebastian Messmer <mail@smessmer.de>
2015-12-18 04:10:10 +0100

    Fix some inconveniences

--- aptly/incontainer_bin/create_repository.sh
+++ aptly/incontainer_bin/create_repository.sh
@@ -5,6 +5,7 @@ OS_DISTRIBUTION=$2
 DEB_DIRECTORY=$3
 
 set -e
+set -v
 
 # Switch to directory of script
 cd ${0%/*}
@@ -13,9 +14,9 @@ aptly repo create -distribution=$OS_DISTRIBUTION -component=main reponame
 aptly repo add reponame $DEB_DIRECTORY
 aptly repo show -with-packages reponame
 aptly snapshot create reponame-snapshot from repo reponame
-aptly snapshot verify reponame-snapshot
+aptly snapshot verify -architectures "amd64,i386" reponame-snapshot
 aptly snapshot list
-aptly publish snapshot reponame-snapshot s3:$OS:
+aptly publish -architectures "amd64,i386" snapshot reponame-snapshot s3:$OS:
 
 set +e
 
--- start_machines.sh
+++ start_machines.sh
@@ -9,4 +9,7 @@ cd vagrant
 vagrant up --provider=google ubuntu-15.10-x64 ubuntu-15.04-x64 ubuntu-14.04-x64 ubuntu-12.04-x64 debian-8-x64
 vagrant up --provider=digital_ocean ubuntu-15.10-x32 ubuntu-15.04-x32 ubuntu-14.04-x32 ubuntu-12.04-x32
 
+# Reload debian machine (needs that because it needs an updated kernel)
+vagrant reload debian-8-x64
+
 set +e
