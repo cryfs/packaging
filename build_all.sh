f62b88e7c4cbd0fe494673b38561f93557259d56
Sebastian Messmer <mail@smessmer.de>
2016-02-14 23:56:23 +0100

    Bring up all machines

--- build_all.sh
+++ build_all.sh
@@ -20,9 +20,10 @@ runbuild ubuntu-15.04-x32 vivid
 runbuild ubuntu-14.04-x64 trusty
 runbuild ubuntu-14.04-x32 trusty
 runbuild ubuntu-12.04-x64 precise
-#runbuild ubuntu-12.04-x32 precise
+runbuild ubuntu-12.04-x32 precise
 
 runbuild debian-8-x64 jessie
+runbuild debian-8-x32 jessie
 
 # Wait until all finished
 wait
--- start_machines.sh
+++ start_machines.sh
@@ -7,7 +7,8 @@ cd ${0%/*}
 
 cd vagrant
 vagrant up --provider=google ubuntu-15.10-x64 ubuntu-15.04-x64 ubuntu-14.04-x64 ubuntu-12.04-x64 debian-8-x64
-vagrant up --provider=digital_ocean ubuntu-15.10-x32 ubuntu-15.04-x32 ubuntu-14.04-x32 ubuntu-12.04-x32
+vagrant up --provider=digital_ocean ubuntu-15.10-x32 ubuntu-14.04-x32 ubuntu-12.04-x32
+vagrant up ubuntu-15.04-x32
 
 # Reload debian machine (needs that because it needs an updated kernel)
 vagrant reload debian-8-x64
