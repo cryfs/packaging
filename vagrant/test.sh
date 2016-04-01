5582b1660191b95388721ff8db9d16df5966368a
Sebastian Messmer <heinzisoft@web.de>
2016-04-02 00:00:11 +0200

    Improve test script output

--- vagrant/test.sh
+++ vagrant/test.sh
@@ -28,23 +28,25 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=on
 make -j$NUMCORES
 
-./test/gitversion/gitversion-test --gtest_also_run_disabled_tests && \
-./test/cpp-utils/cpp-utils-test --gtest_also_run_disabled_tests && \
-./test/parallelaccessstore/parallelaccessstore-test --gtest_also_run_disabled_tests && \
-./test/blockstore/blockstore-test --gtest_also_run_disabled_tests && \
-./test/blobstore/blobstore-test --gtest_also_run_disabled_tests && \
-./test/fspp/fspp-test --gtest_also_run_disabled_tests && \
-./test/cryfs/cryfs-test --gtest_also_run_disabled_tests && \
+./test/gitversion/gitversion-test --gtest_also_run_disabled_tests
+./test/cpp-utils/cpp-utils-test --gtest_also_run_disabled_tests
+./test/parallelaccessstore/parallelaccessstore-test --gtest_also_run_disabled_tests
+./test/blockstore/blockstore-test --gtest_also_run_disabled_tests
+./test/blobstore/blobstore-test --gtest_also_run_disabled_tests
+./test/fspp/fspp-test --gtest_also_run_disabled_tests
+./test/cryfs/cryfs-test --gtest_also_run_disabled_tests
 ./test/cryfs/cryfs-cli-test --gtest_also_run_disabled_tests
 
 # TODO When merging library branch, add these test cases and (probably) remove cryfs-test
-#./test/cryfs/lib_usage_test/cryfs-lib-usage-test --gtest_also_run_disabled_tests && \
-#./test/cryfs/lib_usage_test_cpp/cryfs-lib-usage-test-cpp --gtest_also_run_disabled_tests && \
-#./test/cryfs/lib_link_test/cryfs-lib-link-test  --gtest_also_run_disabled_tests && \
-#./test/cryfs/impl/cryfs-impl-test --gtest_also_run_disabled_tests && \
+#./test/cryfs/lib_usage_test/cryfs-lib-usage-test --gtest_also_run_disabled_tests
+#./test/cryfs/lib_usage_test_cpp/cryfs-lib-usage-test-cpp --gtest_also_run_disabled_tests
+#./test/cryfs/lib_link_test/cryfs-lib-link-test  --gtest_also_run_disabled_tests
+#./test/cryfs/impl/cryfs-impl-test --gtest_also_run_disabled_tests
 
 cd /
 #sudo umount $TMPDIR
 rm -rf $TMPDIR
+
+echo All Tests Finished
 
 set +e
