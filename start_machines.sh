4f1efed1466ed1c6a13e819534343c64a0cfdd6b
Sebastian Messmer <heinzisoft@web.de>
2016-04-29 15:26:43 -0700

    Add debian 32bit to start_machines.sh

--- start_machines.sh
+++ start_machines.sh
@@ -7,10 +7,11 @@ cd ${0%/*}
 
 cd vagrant
 vagrant up --provider=google ubuntu-16.04-x64 ubuntu-15.10-x64 ubuntu-15.04-x64 ubuntu-14.04-x64 ubuntu-12.04-x64 debian-8-x64
-vagrant up --provider=digital_ocean ubuntu-16.04-x32 ubuntu-15.10-x32 ubuntu-14.04-x32 ubuntu-12.04-x32
+vagrant up --provider=digital_ocean ubuntu-16.04-x32 ubuntu-15.10-x32 ubuntu-14.04-x32 ubuntu-12.04-x32 debian-8-x32
 vagrant up ubuntu-15.04-x32
 
 # Reload debian machine (needs that because it needs an updated kernel)
 vagrant reload debian-8-x64
+vagrant reload debian-8-x32
 
 set +e
