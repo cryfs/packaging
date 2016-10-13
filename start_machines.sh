2cbe6df09e9a9998c5b2214c6931161a77a33ed9
Sebastian Messmer <heinzisoft@web.de>
2016-10-13 18:26:06 +0200

    * Run ubuntu 16.10 on digital ocean instead of locally * Disable swap, because stronger cloud machines have enough ram

--- start_machines.sh
+++ start_machines.sh
@@ -7,9 +7,9 @@ cd ${0%/*}
 
 cd vagrant
 vagrant up --provider=google ubuntu-16.04-x64 ubuntu-14.04-x64 ubuntu-12.04-x64 debian-8-x64
-vagrant up --provider=digital_ocean ubuntu-16.04-x32 ubuntu-14.04-x32 ubuntu-12.04-x32 debian-8-x32
-# TODO Once google/digital_ocean images for ubuntu 16.10 are available, use them instead
-vagrant up ubuntu-16.10-x32 ubuntu-16.10-x64
+vagrant up --provider=digital_ocean ubuntu-16.04-x32 ubuntu-14.04-x32 ubuntu-12.04-x32 debian-8-x32 ubuntu-16.10-x64
+# TODO Once google image for ubuntu 16.10 x64 is available, use that instead
+vagrant up ubuntu-16.10-x32
 
 # Reload debian machine (needs that because it needs an updated kernel)
 vagrant reload debian-8-x64
