ae5c5796692709f011ebfabb04e103469fd6fd28
Sebastian Messmer <messmer@cryfs.org>
2019-01-20 14:28:51 -0800

    Update list of supported operating systems

--- start_machines.sh
+++ start_machines.sh
@@ -6,10 +6,10 @@ set -e
 cd ${0%/*}
 
 cd vagrant
-#vagrant up --provider=google ubuntu-14.04-x64 debian-8-x64
-vagrant up --provider=digital_ocean ubuntu-17.10-x64 ubuntu-17.10-x32 ubuntu-17.04-x64 ubuntu-17.04-x32 ubuntu-16.04-x64 ubuntu-16.04-x32 ubuntu-14.04-x64 ubuntu-14.04-x32 debian-9-x64 debian-9-x32 debian-8-x64 debian-8-x32
-# TODO Once google image for ubuntu 16.10 x64 is available, use that instead
-#vagrant up ubuntu-16.10-x32 ubuntu-16.04-x64
+#vagrant up --provider=digital_ocean ubuntu-18.10-x64 ubuntu-18.10-x32 ubuntu-18.04-x64 ubuntu-18.04-x32 ubuntu-16.04-x64 ubuntu-16.04-x32 ubuntu-14.04-x64 ubuntu-14.04-x32 debian-9-x64 debian-9-x32 debian-8-x64 debian-8-x32
+#vagrant up --provider=google ubuntu-18.10-x64 ubuntu-18.10-x32 ubuntu-18.04-x64 ubuntu-18.04-x32 ubuntu-16.04-x64 ubuntu-16.04-x32 ubuntu-14.04-x64 ubuntu-14.04-x32 debian-9-x64 debian-9-x32 debian-8-x64 debian-8-x32
+vagrant up --provider=virtualbox --parallel ubuntu-18.10-x64 ubuntu-18.04-x64 ubuntu-16.04-x64 ubuntu-14.04-x64 debian-9-x64 debian-8-x64
+#vagrant up --provider=digital_ocean ubuntu-18.10-x32 ubuntu-18.04-x32 ubuntu-16.04-x32 ubuntu-14.04-x32 debian-9-x32 debian-8-x32
 
 # Reload debian machine (needs that because it needs an updated kernel)
 vagrant reload debian-8-x64
