814119ccbad95064c928f9aa7bfb1c520551ad65
Sebastian Messmer <messmer@cryfs.org>
2019-01-20 21:06:40 -0800

    Remove 32bit versions of distributions that don't support it anymore

--- start_machines.sh
+++ start_machines.sh
@@ -8,8 +8,7 @@ cd ${0%/*}
 cd vagrant
 #vagrant up --provider=digital_ocean ubuntu-18.10-x64 ubuntu-18.10-x32 ubuntu-18.04-x64 ubuntu-18.04-x32 ubuntu-16.04-x64 ubuntu-16.04-x32 ubuntu-14.04-x64 ubuntu-14.04-x32 debian-9-x64 debian-9-x32 debian-8-x64 debian-8-x32
 #vagrant up --provider=google ubuntu-18.10-x64 ubuntu-18.10-x32 ubuntu-18.04-x64 ubuntu-18.04-x32 ubuntu-16.04-x64 ubuntu-16.04-x32 ubuntu-14.04-x64 ubuntu-14.04-x32 debian-9-x64 debian-9-x32 debian-8-x64 debian-8-x32
-vagrant up --provider=virtualbox --parallel ubuntu-18.10-x64 ubuntu-18.04-x64 ubuntu-16.04-x64 ubuntu-14.04-x64 debian-9-x64 debian-8-x64
-#vagrant up --provider=digital_ocean ubuntu-18.10-x32 ubuntu-18.04-x32 ubuntu-16.04-x32 ubuntu-14.04-x32 debian-9-x32 debian-8-x32
+vagrant up --provider=virtualbox --parallel ubuntu-18.10-x64 ubuntu-18.04-x64 ubuntu-16.04-x64 ubuntu-16.04-x32 ubuntu-14.04-x64 ubuntu-14.04-x32 debian-9-x64 debian-8-x64 debian-8-x32
 
 # Reload debian machine (needs that because it needs an updated kernel)
 vagrant reload debian-8-x64
--- test_all.sh
+++ test_all.sh
@@ -13,19 +13,14 @@ function runtest {
   ./test.sh $1 $TAG > build/test-$1.stdout 2> build/test-$1.stderr &
 }
 
-runtest ubuntu-17.10-x64
-#runtest ubuntu-17.10-x32
-#runtest ubuntu-17.04-x64
-#runtest ubuntu-17.04-x32
-#done runtest ubuntu-16.04-x64
-#done runtest ubuntu-16.04-x32
-#done runtest ubuntu-14.04-x64
-#done runtest ubuntu-14.04-x32
-
+runtest ubuntu-18.10-x64
+runtest ubuntu-18.04-x64
+runtest ubuntu-16.04-x64
+runtest ubuntu-16.04-x32
+runtest ubuntu-14.04-x64
+runtest ubuntu-14.04-x32
 runtest debian-9-x64
-#runtest debian-9-x32
-#done runtest debian-8-x64
-#done runtest debian-8-x32
+runtest debian-8-x64
 
 # Wait until all finished
 wait
