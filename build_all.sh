3e3a6ce7f384326c1959e09e15b7f63465f27c85
Sebastian Messmer <messmer@cryfs.org>
2021-04-02 13:27:12 -0700

    Update

--- build_all.sh
+++ build_all.sh
@@ -13,14 +13,11 @@ function runbuild {
   ./build.sh $1 $TAG $2 > build/build-$1.stdout 2> build/build-$1.stderr &
 }
 
-runbuild ubuntu-18.10-x64
+runbuild ubuntu-20.10-x64
+runbuild ubuntu-20.04-x64
 runbuild ubuntu-18.04-x64
-runbuild ubuntu-16.04-x64
-runbuild ubuntu-16.04-x32
-runbuild ubuntu-14.04-x64
-runbuild ubuntu-14.04-x32
+runbuild debian-10-x64
 runbuild debian-9-x64
-runbuild debian-8-x64
 
 # Wait until all finished
 wait
--- test_all.sh
+++ test_all.sh
@@ -13,14 +13,11 @@ function runtest {
   ./test.sh $1 $TAG > build/test-$1.stdout 2> build/test-$1.stderr &
 }
 
-runtest ubuntu-18.10-x64
+runtest ubuntu-20.10-x64
+runtest ubuntu-20.04-x64
 runtest ubuntu-18.04-x64
-runtest ubuntu-16.04-x64
-runtest ubuntu-16.04-x32
-runtest ubuntu-14.04-x64
-runtest ubuntu-14.04-x32
+runtest debian-10-x64
 runtest debian-9-x64
-runtest debian-8-x64
 
 # Wait until all finished
 wait
